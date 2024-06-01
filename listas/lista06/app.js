ed68b99d24a0e72f6cac67c0c7ef55bf45da8efa
MarcusViniciusDev02 <m.v.paixaoo@gmail.com>
2024-05-31 21:49:14 -0300

    lista06

---
+++ listas/lista06/app.js
@@ -0,0 +1,52 @@
+var pix = {
+    chavePix: 489304802934,
+    valorTransferencia: 20.03,
+    dataTransacao: "20/03/2024",
+    descricaoPagamento: "Realizado com sucesso",
+    pagador:[
+        {
+            nome: "Marcus Vinicius",
+            cpf: 123204324234
+        }
+    ],
+    recebedor:[
+        {
+            nome: "Joaquin Otávio",
+            cpf: 123204324234
+        }
+    ]
+}
+
+var historicoTransacoes= {
+    transacoes:[
+        {
+            data: "20/04/2023",
+            valor: 1000.00,
+            descricao: "Realizado com sucesso;"
+        },
+        {
+            data: "20/06/2023",
+            valor: 1000.00,
+            descricao: "Realizado com sucesso;"
+        },
+        {
+            data: "10/04/2023",
+            valor: 1000.00,
+            descricao: "Realizado com sucesso;"
+        },
+        {
+            data: "30/07/2023",
+            valor: 1000.00,
+            descricao: "Realizado com sucesso;"
+        },
+        {
+            data: "10/08/2023",
+            valor: 1000.00,
+            descricao: "Realizado com sucesso;"
+        }
+        
+    ]
+}
+
+console.log(pix)
+console.log(historicoTransacoes)
